133da78a50ef15011af04de57f8bfe07bf54663c
v-anderson <54355706+v-anderson@users.noreply.github.com>
2020-07-14 22:41:18 +0700

    Pertama kali commit

---
+++ MC3_14_ProjectKura/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MC3_14_ProjectKura
+//
+//  Created by Vincent Anderson Ngadiman on 14/07/20.
+//  Copyright © 2020 Vincent Anderson Ngadiman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
