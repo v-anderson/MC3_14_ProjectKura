b91911a4be6910a404cc2e40cd5197be35e3ea3c
v-anderson <54355706+v-anderson@users.noreply.github.com>
2020-07-21 21:21:37 +0700

    Layouting pantai bersih

---
+++ MC3_14_ProjectKura/Storyboards/PantaiViewController.swift
@@ -0,0 +1,23 @@
+//
+//  PantaiViewController.swift
+//  MC3_14_ProjectKura
+//
+//  Created by Vincent Anderson Ngadiman on 21/07/20.
+//  Copyright © 2020 Vincent Anderson Ngadiman. All rights reserved.
+//
+
+import UIKit
+
+class PantaiViewController: UIViewController {
+
+    @IBOutlet weak var pantaiBG: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+
+
+}
